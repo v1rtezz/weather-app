37c48992eebfd25a45eb5e6079f0d1b58be4cba3
ThunDer <thunder00104@gmail.com>
2025-07-11 15:39:50 +0300

    Create html css base of app

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+const COMPRESSION_QUALITY = 75;
+
+export default defineConfig({
+  root: "./",
+  base: "./",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+  plugins: [
+    ViteImageOptimizer({
+      png: { quality: COMPRESSION_QUALITY },
+      jpeg: { quality: COMPRESSION_QUALITY },
+      jpg: { quality: COMPRESSION_QUALITY },
+    }),
+  ],
+});
