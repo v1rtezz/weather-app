9097601bd767801f83aa221cfdb36d191f59e6cc
ThunDer <thunder00104@gmail.com>
2025-07-14 12:03:10 +0300

    Refactor main.js

--- src/main.ts
+++ src/main.ts
@@ -4,34 +4,33 @@ import './css/settings.css'
 import './css/app.css'
 
 import { CONFIG } from './config/index'
-
+import { Api } from './api/api'
 import { Modal } from './modules/modal'
-const settingsModal = new Modal(
-  '[data-settings-modal]',
-  '[data-open-settings-modal]',
-  '[data-close-settings-modal]',
-  'settings-window--active',
-  'no-scroll'
-)
-settingsModal.init()
-
+import { renderHoursWeather } from './render/renderHoursWeather'
+import { renderCurrentWeather } from './render/renderCurrentWeather'
+import { renderDaysWeather } from './render/renderDaysWeather'
+import { renderTimeOfDayIcon } from './modules/renderTimeOfDayIcon'
 
 // FIXME: убрать await
 const city = encodeURIComponent('Москва')
 const API_URL_WITH_PARAMS = `${CONFIG.API_BASE_URL}?q=${city}&appid=${CONFIG.API_KEY}&units=metric&lang=ru`
-import { Api } from './api/api'
 const weatherApi = new Api(API_URL_WITH_PARAMS)
 
-import { renderCurrentWeather } from './render/renderCurrentWeather'
 const data = await weatherApi.getData()
-renderCurrentWeather(data)
 
-import { renderHoursWeather } from './render/renderHoursWeather'
-renderHoursWeather(data)
 
-import { renderDaysWeather } from './render/renderDaysWeather'
-renderDaysWeather(data)
-
-import { renderTimeOfDayIcon } from './modules/renderTimeOfDayIcon'
-renderTimeOfDayIcon(data)
-
+const appInit = () => {
+  const settingsModal = new Modal(
+    '[data-settings-modal]',
+    '[data-open-settings-modal]',
+    '[data-close-settings-modal]',
+    'settings-window--active',
+    'no-scroll',
+  )
+  settingsModal.init()
+  renderCurrentWeather(data)
+  renderHoursWeather(data)
+  renderDaysWeather(data)
+  renderTimeOfDayIcon(data)
+}
+appInit()
